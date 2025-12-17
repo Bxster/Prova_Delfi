3039a190fe38a029c89dd23ab3089a9ce0353ace
Bxster <baldellimarketing@gmail.com>
2025-12-17 12:08:50 +0100

    5 minuti

--- software/V_TFLite/config.py
+++ software/V_TFLite/config.py
@@ -65,7 +65,7 @@ LOG_DATE_FMT = "%Y%m%d"
 
 # --- Continuous Recording ---
 CONTINUOUS_RECORDING_ENABLED = True  # Set to False to disable continuous recording
-MAX_RECORDING_DURATION_SEC = 900  # Max 15 min (900 sec) to avoid huge files
+MAX_RECORDING_DURATION_SEC = 300  # Max 5 min (300 sec) to avoid huge files
 CONTINUOUS_RECORDING_DIR = f"{LOGS_DIR}/continuous_recordings"
 
 # --- Window Saving (Debug/Analysis) ---
